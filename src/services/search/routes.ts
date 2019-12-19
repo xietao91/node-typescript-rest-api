b1d7d23454b3c0ab1faabc3055843f394313e25c
xietao <931864989@qq.com>
2019-12-19 10:38:35 +0800

    完善step4代码

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
 
 export default [
   {
-    path:'/',
-    method:'get',
-    async handler(req: Request, rep: Response) {
-      rep.send('hello wold');
-    }
+    path: "/api/v1/search",
+    method: "get",
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q);
+        res.status(200).send(result);
+      }
+    ]
   }
 ];
