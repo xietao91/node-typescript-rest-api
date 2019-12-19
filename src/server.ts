3ea1efef323a088b977b55e94c15b37005d4af65
xietao <931864989@qq.com>
2019-12-19 09:57:35 +0800

    step3

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,24 @@ import express from 'express';
 import { applyMiddleware, applyRoutes } from './utils';
 import middleware from './middleware';
 import searchRouters from './services';
+import errorHandlers from './middleware/errorHandlers';
 
 const router = express();
 applyMiddleware(middleware, router);
 applyRoutes(searchRouters, router);
+applyMiddleware(errorHandlers, router);
 const {PORT = 3000} = process.env;
 const server = http.createServer(router);
 
 server.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
+});
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
 });
